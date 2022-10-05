23ebf3bbfa248b44676d4d4f493ea04b043339d3
jawFrome <james_wraight@hotmail.com>
2022-10-05 15:19:06 +0100

    updated to run main methods from main

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,5 +1,9 @@
 mod mandlebrot;
+use num_complex::Complex64;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", mandlebrot::is_in_range(Complex64{re: 0.5, im:0.1}));
+    println!("{}", mandlebrot::is_in_range(Complex64{re: 3.0, im:0.1}));
+
+    mandlebrot::get_image_of_range(Complex64{re:-2.0, im:2.0}, Complex64{re:2.0, im:-2.0});
 }
--- rust-api/src/mandlebrot.rs
+++ rust-api/src/mandlebrot.rs
@@ -1,67 +1,62 @@
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Complex {
-    pub real: f32,
-    pub imag: f32,
+use num_complex::Complex64;
+
+pub fn upper_iteration_limit() -> i32{
+    255
 }
 
-impl Complex {
-    pub fn new(f32:real, f32:imag) -> self{
-        self{
-            real,
-            imag,
-        }
-    }
+pub fn upper_squared_limit() -> i32{
+    4
 }
 
-pub fn is_in_the_set(complex: value) -> boolean {
-    if (value.real.abs() > 2 || value.imag.abs() > 2){
+pub fn is_in_the_set(value: Complex64) -> bool {
+    if value.re.abs() > 2.0 || value.im.abs() > 2.0 {
                 return false;            
     }
 
-    return !(Process(value) < UpperIterationLimit);
+    return !(process(value) < upper_iteration_limit());
 }
 
-pub fn get_image_of_range(complex: upperLeft, complex: lowerRight) -> () {
-    let UpperIterationLimit = 255;
-    let UpperSquaredLimit: i32 = 4;
-    let Height = 800;
-    let Width = 800;
+pub fn get_image_of_range(upper_left: Complex64, lower_right: Complex64) -> () {
+    const HEIGHT: usize = 800;
+    const WIDTH: usize = 800;
 
-    let x_resolution = (lowerRight.real - upperLeft.real) / Width;
-    let y_resolution = (upperLeft.imag - lowerRight.imag) / Height;
-    let depthValues : &[[u32; Height]; Width];
+    let x_resolution = (lower_right.re - upper_left.re) / (WIDTH as f64);
+    let y_resolution = (upper_left.im - lower_right.im) / (HEIGHT as f64);
+    let mut depth_values = vec![vec![0; WIDTH]; HEIGHT];
     let mut x = 0;
-    let mut real = upperLeft.real;
-    while  real < lowerRight.real {
+    let mut real = upper_left.re;
+    while  real < lower_right.re
+    {
         let mut y = 0;
-        let mut imag = lowerRight.imag;
-        while imag < upperLeft.imag{
-            depthValues[x][y] = Process(complex(real, imag));
+        let mut imag = lower_right.im;
+        while imag < upper_left.im 
+        {
+            depth_values[x][y] = process(Complex64{re: real, im: imag});
+            
             y = y + 1;
-            if y >= Height{
+            if y >= HEIGHT{
                 break;
             }
-        
+
+            imag = imag + y_resolution
+        }
+
         x = x + 1;
-        if x >= Width{
+        if x >= WIDTH{
             break;
         }  
 
-        imag = imag + y_resolution
+        real = real + x_resolution;    
     }
-
-    real = real + x_resolution;
+    
 }
 
-    return createImageFromArrayAsRawBytes(depthValues);       
-}
 
-pub fn Process(complex: value) -> int {
-    c = complex(real, imag);
-    depth = 0;
-    Zn = complex(0, 0);
-    while (IsInRange(Zn) && depth < UpperIterationLimit){
-        Zn = Zn * Zn + c;
+pub fn process(value: Complex64) -> i32 {
+    let mut depth = 0;
+    let mut zn = Complex64{re:0f64, im:0f64};
+    while is_in_range(zn) && depth < upper_iteration_limit() {
+        zn = zn * zn + value;
         depth = depth + 1;
     }
     
@@ -69,7 +64,7 @@ pub fn Process(complex: value) -> int {
 }
 
 
-pub fn IsInRange(complex: value) -> boolean {
-    polarForm = cmath.polar(value);
-    return (pow(polarForm[0], 2) <= UpperSquaredLimit);
+pub fn is_in_range(value: Complex64) -> bool {
+    let polar_form = value.to_polar();
+    return polar_form.0.powf(2.0) <= upper_squared_limit() as f64;
 }
